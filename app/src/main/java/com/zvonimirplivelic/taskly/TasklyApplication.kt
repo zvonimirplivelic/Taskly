9100401232ed51e1ce693d373200ab269c415981
Zvonimir <zvonimirplivelic91@gmail.com>
2021-11-26 14:07:04 +0100

    Setting up navigation

---
+++ app/src/main/java/com/zvonimirplivelic/taskly/TasklyApplication.kt
@@ -0,0 +1,4 @@
+package com.zvonimirplivelic.taskly
+
+class TasklyApplication {
+}
---
+++ app/src/main/java/com/zvonimirplivelic/taskly/ui/fragment/TaskListFragment.kt
@@ -0,0 +1,23 @@
+package com.zvonimirplivelic.taskly.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.zvonimirplivelic.taskly.R
+
+
+class TaskListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_task_list, container, false)
+    }
+}
